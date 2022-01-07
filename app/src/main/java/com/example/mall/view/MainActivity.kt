c23f89dfa659dcef486d3a685d9e1928239112fa
meiqi <805560812@qq.com>
2022-01-07 16:15:51 +0800

    init environment and app theme

--- app/src/main/java/com/example/mall/view/MainActivity.kt
+++ app/src/main/java/com/example/mall/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mall
+package com.example.mall.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mall.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
